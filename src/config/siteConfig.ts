4b0deab3e58daa38185bbebb973f2df1cb80c9d0
wliuy <51187046+wliuy@users.noreply.github.com>
2026-03-18 21:25:29 +0800

    Update siteConfig.ts

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -7,26 +7,25 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "8972",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "技术与资源分享",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://blog.89729981.xyz",
 
 	// 站点描述
 	description:
-		"Firefly 是一款基于 Astro 框架和 Fuwari 模板开发的清新美观且现代化个人博客主题模板，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
+		"8972 的个人技术博客。专注于优质资源分享、自托管服务指南以及各种技术方案的探索记录，致力于打造一个纯粹的技术沉淀空间。",
 
 	// 站点关键词
 	keywords: [
-		"Firefly",
-		"Fuwari",
-		"Astro",
-		"ACGN",
-		"博客",
+		"8972",
 		"技术博客",
+		"资源分享",
+		"自托管",
+		"Self-hosting",
 		"静态博客",
 	],
 
@@ -79,7 +78,7 @@ export const siteConfig: SiteConfig = {
 			alt: "🍀",
 		},
 		// 导航栏标题
-		title: "Firefly",
+		title: "8972",
 		// 全宽导航栏，导航栏是否占满屏幕宽度
 		widthFull: false,
 		// 导航菜单对齐方式，left：左对齐，center：居中
