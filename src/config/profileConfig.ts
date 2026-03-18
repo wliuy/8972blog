9da55194a70bc954cb0de940a2ac5ab1aa21019f
wliuy <51187046+wliuy@users.noreply.github.com>
2026-03-18 21:34:59 +0800

    Update profileConfig.ts

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -9,10 +9,10 @@ export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.avif",
 
 	// 名字
-	name: "Firefly",
+	name: "8972",
 
 	// 个人签名
-	bio: "Hello, I'm Firefly.",
+	bio: "Hello, I'm 8972.",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
@@ -25,25 +25,25 @@ export const profileConfig: ProfileConfig = {
 			name: "qq",
 			icon: "fa7-brands:qq",
 			url: "https://qm.qq.com/q/ZGsFa8qX2G",
-			showName: false,
+			showName: true,
 		},
 		{
 			name: "GitHub",
 			icon: "fa7-brands:github",
-			url: "https://github.com/CuteLeaf",
-			showName: false,
+			url: "https://github.com/wliuy",
+			showName: true,
 		},
 		{
 			name: "Email",
 			icon: "fa7-solid:envelope",
 			url: "mailto:xiaye@msn.com",
-			showName: false,
+			showName: true,
 		},
 		{
 			name: "RSS",
 			icon: "fa7-solid:rss",
 			url: "/rss/",
-			showName: false,
+			showName: true,
 		},
 	],
 };
